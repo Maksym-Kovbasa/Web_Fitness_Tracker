900de00c9f3e8d2425bbf8e536a686eba45e582d
Maksym-Kovbasa <maximkovbasa2222@gmail.com>
2025-05-24 11:05:50 +0300

    Creation of entities Goal, Workout

--- demo/src/main/java/com/example/demo/Model/Goal.java
+++ demo/src/main/java/com/example/demo/Model/Goal.java
@@ -1,5 +1,26 @@
 package com.example.demo.Model;
 
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
 public class Goal {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private int targetCalories;
+    private int targetWorkouts;
     
+    private LocalDate startDate;
+    private LocalDate endDate;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 }
--- demo/src/main/java/com/example/demo/Model/Workout.java
+++ demo/src/main/java/com/example/demo/Model/Workout.java
@@ -1,5 +1,26 @@
 package com.example.demo.Model;
 
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
 public class Workout {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String type;
+    private int duration;
+    private int calories;
     
+    private LocalDate date;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 }
